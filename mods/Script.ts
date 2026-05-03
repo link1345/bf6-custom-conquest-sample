67a5b4874c65d53360656c62897b7085052138b0
link1345 <testkusa@gmail.com>
2026-05-03 11:33:10 +0900

    remove

--- mods/Script.ts
+++
@@ -1,7 +0,0 @@
-import * as modlib from "modlib";
-import { createTicketHud } from "./AppleInFront";
-
-export function OnPlayerJoinGame(eventPlayer: mod.Player) {
-    modlib.ShowNotificationMessage(mod.Message(mod.stringkeys.hello, 1));
-    createTicketHud(eventPlayer);
-}
